5fa0961efb906c2a65a41dae1182f8c4ab04abf6
Jefferson Vitor de Santana <computadores.rp@gmail.com>
2022-04-09 13:13:20 -0300

    Aprendendo a debugar.

--- grita.sh
+++ grita.sh
@@ -6,6 +6,8 @@
 
 TXT="gritaria"
 TXT="     $TXT     "          # Adiciona 5 espaços ao redor
-TXT=$(echo $TXT | tr '''!')   # Troca os espaços por exclamções
+echo "TXT com espaços: [$TXT]"
+TXT=$(echo "$TXT" | tr ' ' '!')   # Troca os espaços por exclamções
+echo "TXT com exclamções: [$TXT]"
 TXT=$(echo $TXT | tr a-z A-Z) # Deixa o texto em maiúsculas
 echo "$TXT"                   # Mostra a mensagem
