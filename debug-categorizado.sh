460442d64e4ba34d6b9f764578380710d154c7b6
Jefferson Vitor de Santana <computadores.rp@gmail.com>
2022-04-09 15:37:28 -0300

    Programa para testar o debug categorizado. Nome: debug-categorizado.sh

---
+++ debug-categorizado.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env bash
+
+# debug-categorizado.sh
+
+#
+
+# Exemplo de Debug categorizado em três níveis
+
+DEBUG=${1:-0} # passe o nível pelo $1
+
+Debug(){
+  [ $1 -le $DEBUG ] && echo "--- DEBUG $*"
+}
+
+Debug 1 "Início do Programa"
+
+i=0
+
+max=5
+
+echo "Contando até $max"
+
+Debug 2 "Vou entrar no WHILE"
+
+while [ $i -ne $max]; do
+  Debug 3 "Valor de \$i antes de incrementar: $i"
+  let i=$i+1
+  echo "$i..."
+done
+
+Debug 2 "Saí do WHILE"
+
+echo 'Terminei!'
+
+Debug 1 "Fim do Programa"
