524cf48243afe8f963132e5ec84b558a339b7be5
Jefferson Vitor de Santana <computadores.rp@gmail.com>
2022-07-09 14:23:46 -0300

    Colocado if para testar se existe o aquivo de conviguração e se esta legível.

--- capitulo_9/parser.sh
+++ capitulo_9/parser.sh
@@ -5,3 +5,16 @@
 #
 # 2022-9-7 Jefferson Santana
 #
+# O arquivo de configuração é indicado na linha de comando
+
+CONFIG=$1
+
+# O arquivo deve existir e ser legível
+
+if [ -z "$CONFIG" ]; then
+  echo "Uso: parser arquivo.conf"
+  exit 1
+elif [ ! -r "$CONFIG" ]; then
+  echo "Erro: Não consigo ler o arquivo $CONFIG"
+  exit 1
+fi
